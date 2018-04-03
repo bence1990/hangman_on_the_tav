ade9d3a29bf7cd4020d3933070a97903fe142851
bence <bencekollar23@gmail.com>
2018-04-03 14:27:16 +0200

    added main

--- gyakorlat.py
+++ gyakorlat.py
@@ -2,20 +2,26 @@ import random
 
 
 def random_genearted_word(my_list):
-    value=random.randint(0,len(my_list)-1)
+    value = random.randint(0, len(my_list)-1)
     return my_list[value]
 
-         
+
 def file_reading(file_name):
     with open(file_name, "r") as my_list:
         for item in my_list:
             my_list = item.split(',')
     return my_list
 
+def if_input_in_list(guess_char,get_guess):
+    typed_char = []
+    while True:
+        if get_guess in typed_char:
+            print ("Sorry...Try something else")
+            get_guess
+        
+        
+        
 
-
-def main(): 
-    print (random_genearted_word(file_reading("lista.txt")))
 
 if __name__ == '__main__':
     main()
---
+++ main.py
@@ -0,0 +1,8 @@
+import gyakorlat
+
+def main():
+    print(gyakorlat.random_genearted_word(gyakorlat.file_reading("lista.txt")))
+
+
+
+main()
