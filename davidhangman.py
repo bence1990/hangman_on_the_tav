eb521ecf1ec4828867c8495259a7aca0b835c282
davidlenner <david910128@gmail.com>
2018-04-03 14:52:56 +0200

    fixed input

--- davidhangman.py
+++ davidhangman.py
@@ -1,6 +1,8 @@
 def get_guess():
-    guess_list = []
-    while len(guess_list) != 1:
+    guess = ""
+    while len(guess) != 1:
         guess = input("Write your guess: ")
-        guess_list.append(guess)
-    return guess_list
+    return guess
+
+
+print(get_guess())
