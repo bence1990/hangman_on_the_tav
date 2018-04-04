fc80ed5b83577e80c8f37077701522dd15cd05a7
pannapollak <you@example.com>
2018-04-04 15:30:20 +0200

    same letter in word bug fixed

--- gyakorlat.py
+++ gyakorlat.py
@@ -3,15 +3,15 @@ import sys
 import os
 
 
-def random_genearted_word(my_list):
+def generate_random_word(my_list):
     value = random.randint(0, len(my_list)-1)
     return my_list[value]
 
 
-def file_reading(file_name):
+def read_file(file_name):
     with open(file_name, "r") as my_list:
         for item in my_list:
-            my_list = item.split(',')
+            my_list = item.split(' ')
     return my_list
 
 
@@ -23,27 +23,33 @@ def get_guess():
 
 
 def print_underscore(random_word):
-    underscore = len(random_word) * '-'
+    underscore = len(random_word) * '*'
     return underscore
 
 
 def check_guess(random_word, guess):
+    index_list = []
     if guess in random_word:
         for random in range(0, len(random_word)):
             if random_word[random] == guess:
-                return random
+                index_list.append(random)
+        return index_list
     else:
         return False
 
 
-def fill_underscore(random, underscore, guess):
-    underscore[random] = guess
+def fill_underscore(index_list, underscore, guess):
+    underscore = list(underscore)
+    for index in index_list:
+        del underscore[index]
+        underscore.insert(index, guess)
+    underscore = ''.join(underscore)
     return underscore
 
 
 def printing_board():
     os.system("clear")
-    hangman_list=[" "," "," "," "," "," "," "," "," "," "," "]
+    hangman_list = [" "," "," "," "," "," "," "," "," "," "," "]
 
     print(   hangman_list[7],hangman_list[8],hangman_list[9] )
     print(   hangman_list[6],'             ',hangman_list[10])
@@ -57,14 +63,14 @@ def printing_board():
 
 def main():
     guess = get_guess()
-    random_word = 'bull'  # random_genearted_word(file_reading("lista.txt"))
-    print(print_underscore(random_word))
+    random_word = generate_random_word(read_file("lista.txt"))
     underscore = print_underscore(random_word)
-    while check_guess(random_word, guess) is False:
-        print(printing_board())
-        get_guess()
-    random = check_guess(random_word, guess)
-    print(fill_underscore(random, underscore, guess))
+    while True:
+        while check_guess(random_word, guess) is False:
+            print(printing_board())
+            get_guess()
+        index_list = check_guess(random_word, guess)
+        print(fill_underscore(index_list, underscore, guess))
 
 
 if __name__ == '__main__':
